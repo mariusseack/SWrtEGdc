403a57a67a08de63cfed33b6068d443aa5001eb2
mariusseack <marius.seack@cupsociety.de>
2017-12-11 23:46:08 +0100

    Redesign Project
    Ich habe das aktuelle Projekt hochgeladen. Bitte damit arbeiten

---
+++ js/custom.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+  $(".button-collapse").sideNav();
+  $('.collapsible').collapsible();
+  $('.tap-target').tapTarget('close');
+});
+
+$(document).scroll(function() {
+  var nav = $("nav");
+
+  if ($(this).scrollTop() > 0 && !nav.hasClass("inverse")) {
+    nav.removeClass("transparent");
+    nav.addClass("scrolled-nav");
+    nav.addClass("z-depth-4");
+  } else if(!nav.hasClass("inverse")) {
+    nav.addClass("transparent");
+    nav.removeClass("scrolled-nav");
+    nav.removeClass("z-depth-4");
+  }
+});
+
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true
+});
+
+//hides the toHide element and shows the toShow element. Easy
+function toggleHidness(toHide, toShow){
+  if (toHide != "") {
+    $(toHide).hide();
+  }
+
+  if (toShow != ""){
+    if ($(toShow).hasClass("hide")) {
+      console.log('i am hidden');
+      $(toShow).removeClass("hide");
+    }
+    else {
+      $(toShow).show();
+    }
+  }
+}
