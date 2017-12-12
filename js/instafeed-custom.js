41ff4b79b4b51a9caa8185de3004d42a366287bc
mariusseack <marius.seack@cupsociety.de>
2017-12-12 17:51:37 +0100

    navbar edit and button_group

--- js/instafeed-custom.js
+++ js/instafeed-custom.js
@@ -8,15 +8,15 @@ $(document).ready(function() {
     sortBy: 'most-recent',
     template: '<a href="instagram://media?id={{id}}" target="blank"><div class="img-wrapper pointer"><div class="img-overlay"><i class="fa fa-heart fa-4x center-align" aria-hidden="true"></i></div><img src="{{image}}" class="img"></div></a>',
     before: function() {
-      $(".load-instafeed-group>button").addClass("hide");
-      $(".load-instafeed-group>div").removeClass("hide");
+      $(".load-instafeed-group>div.btn_group").addClass("hide");
+      $(".load-instafeed-group>div.spinner_group").removeClass("hide");
     },
     after: function() {
-      $(".load-instafeed-group>div").addClass("hide");
-      $(".load-instafeed-group>button").removeClass("hide");
+      $(".load-instafeed-group>div.spinner_group").addClass("hide");
+      $(".load-instafeed-group>div.btn_group").removeClass("hide");
       // disable button if no more results to load
       if (!this.hasNext()) {
-        $(".load-instafeed-group>button").addClass("hide");
+        $(".load-instafeed-group>div.btn_group").addClass("hide");
       }
     },
   });
@@ -24,7 +24,7 @@ $(document).ready(function() {
   //template: '<div class="col-lg-3 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>'
   userFeed.run();
 
-  $(".load-instafeed-group>button").on("click", function() {
+  $(".load-instafeed-group>div.btn_group>button").on("click", function() {
     userFeed.next()
   });
 
