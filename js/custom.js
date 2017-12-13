f168b760b9ba51bc81473344d8ddc9e9e4e545a1
mariusseack <marius.seack@cupsociety.de>
2017-12-13 16:49:13 +0100

    tea-template: header

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,12 @@
+var nav = $("nav");
+var ph_container = $('.parallax-container .heading-container');
+var para_element = ph_container.children();
+var ele_offset = para_element.offset().top - para_element.height();
+var puffer = 10;    //amount (in pixel) of puffer the element can scroll above top
+var max_offset = ele_offset + puffer;
+
+console.log(ph_container);
+
 $(document).ready(function() {
   $(".button-collapse").sideNav();
   $('.collapsible').collapsible();
@@ -6,17 +15,27 @@ $(document).ready(function() {
 });
 
 $(document).scroll(function() {
-  var nav = $("nav");
-
   if ($(this).scrollTop() > 0 && !nav.hasClass("inverse")) {
     nav.removeClass("transparent");
     nav.addClass("scrolled-nav");
     nav.addClass("z-depth-4");
-  } else if(!nav.hasClass("inverse")) {
+  } else if (!nav.hasClass("inverse")) {
     nav.addClass("transparent");
     nav.removeClass("scrolled-nav");
     nav.removeClass("z-depth-4");
   }
+
+  //parallax custom scrolleffect
+  var scroll_offset = $(window).scrollTop();
+  var val = (max_offset - scroll_offset);
+  var res = Math.round((val/max_offset) * 100) / 100;
+
+  if(res >= 0){
+    para_element.css({"opacity": res});
+  }
+
+  console.log(res);
+
 });
 
 $('.carousel.carousel-slider').carousel({
@@ -24,17 +43,16 @@ $('.carousel.carousel-slider').carousel({
 });
 
 //hides the toHide element and shows the toShow element. Easy
-function toggleHidness(toHide, toShow){
+function toggleHidness(toHide, toShow) {
   if (toHide != "") {
     $(toHide).hide();
   }
 
-  if (toShow != ""){
+  if (toShow != "") {
     if ($(toShow).hasClass("hide")) {
       console.log('i am hidden');
       $(toShow).removeClass("hide");
-    }
-    else {
+    } else {
       $(toShow).show();
     }
   }
